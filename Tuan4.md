2f46a32f1d591ee43689a9a5bcf8a29ba74e155a
Anhphap2004 <phapmeo2k4@gmail.com>
2024-09-27 18:57:33 +0700

    up code nè

---
+++ Tuan4.md
@@ -0,0 +1,10 @@
+>> Tiếng Anh Chuyên Ngành CNTT <<
+😺 Anhh Phapp
+🦮 Nghĩa
+🐸 Tú
+🐭 Vi
+🦉 Tươi
+🐂 Việt
+🦧 Trọng
+🐄 Thủy
+🐴 Quân
---
+++ phap.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout <<"Nhap a = ";
+    cin >> n;
+    if(n>10)
+    {
+        cout << "Ngghia la con cho";
+    }
+    else{
+        cout <<"Nghia la con meo";
+    }
+}
